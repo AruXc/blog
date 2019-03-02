3784a63d1e8ce3a340451c4ba40aabf223285a2b
AruXc <aruxc6030@gmail.com>
2019-03-02 16:30:52 +0800

    first commit

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,9 @@
+---
+title: Hello World
+date: "2019-03-01T00:00:00Z"
+description: The first post
+---
+
+Time to study!
+
+
---
+++ src/components/bio.js
@@ -0,0 +1,69 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        console.log(data.site.siteMetadata)
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                minWidth: 50,
+                borderRadius: `100%`,
+              }}
+              imgStyle={{
+                borderRadius: `50%`,
+              }}
+            />
+            <p>I am Aru, a front-end developer, welcome my note blog.</p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          github
+        }
+      }
+    }
+  }
+`
+
+export default Bio
